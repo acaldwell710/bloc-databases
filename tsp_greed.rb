8a53ce70b3c78eb25fab250d99717a0b4b818fd6
Amber Caldwell <acaldwell710@gmail.com>
2019-02-04 15:59:01 -0500

    updated tsp

--- tsp_greed.rb
+++ tsp_greed.rb
@@ -2,6 +2,12 @@ def nearest_possible_neighbor(current_city)
 
   neighbor_cities = current_city.neighbors
   nearest_neighbor = neighbor_cities[0]
+  current_city = City.new("Roanoke", 0, 0)
+  city2 = City.new("Washington D.C.", 1, 1)
+  city3 = City.new("Boston", 4, 3)
+  city4 = City.new("New York City", 3, 2)
+  city5 = City.new("Pittsburgh",0, 2)
+  neighbors = [city2, city3, city4, city5]
 
   for current_neighbor in neighbor_cities
     if current_neighbor.visited = FALSE && current_neighbor.distance < nearest_neighbor.distance
@@ -11,8 +17,8 @@ def nearest_possible_neighbor(current_city)
   nearest_neighbor
 end
 
-def distance
-  nearest_neighbor.value - current_city.value
+def distance(loc1x, loc1y, loc2x, loc2y)
+  Math.hypot((loc2x - loc1x), (loc2y - loc1y))
 end
 
 def visited
